159201007c264e853dc7a46f2d8015e89ad3cef3
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-04-16 20:56:56 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["No Skill Customs","Vehicle Works","Maps Works","Cinematics Works"];
 
 
 let currentIndex = 0;
